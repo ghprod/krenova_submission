df482955c6479b246fb412577aa41a34513f3526
raka aditya <adm@rakaaditya.com>
2015-11-26 18:34:58 +0700

    Add gedrix logo

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,6 +11,12 @@
             <br/><br/>
             <h3>Kamu geek banget?</h3>
             <a class="btn btn-lg bg-black btn-social" href="{{url('auth/github')}}"><i class="fa fa-github"></i> <span>Masuk lewat Github</span></a>
+            <br/><br/>
+            <hr/>
+            <center>
+                Didukung oleh:<br/>
+                <a href="http://www.gedrix.com" target="_blank"><img src="http://cdn-ck.gedrix.net/assets/img/logo_gedrix.png" width="150" /></a>
+            </center>
         </div>
     </div>
 @endsection
