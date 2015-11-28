99180407f6bdb72d3c41509c1db592f8e49b2d90
raka aditya <adm@rakaaditya.com>
2015-11-28 23:29:42 +0700

    Add pipe

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,12 +6,12 @@
     <div class="mdl-grid">
         <div class="mdl-cell mdl-cell--4-col" align="center" style="margin:auto;">
             <h3>Masuk</h3>
-            <a class="mdl-button mdl-button--colored mdl-button--raised mdl-js-button mdl-js-ripple-effect bg-blue btn-social" href="{{url('auth/facebook')}}"><i class="fa fa-facebook"></i> <span>Masuk lewat Facebook</span></a>
+            <a class="mdl-button mdl-button--colored mdl-button--raised mdl-js-button mdl-js-ripple-effect bg-blue btn-social" href="{{url('auth/facebook')}}"><i class="fa fa-facebook"></i> | <span>Masuk lewat Facebook</span></a>
             <br/><br/>
-            <a class="mdl-button mdl-button--colored mdl-button--raised mdl-js-button mdl-js-ripple-effect bg-light-blue btn-social" href="{{url('auth/twitter')}}"><i class="fa fa-twitter"></i> <span>Masuk lewat Twitter</span></a>
+            <a class="mdl-button mdl-button--colored mdl-button--raised mdl-js-button mdl-js-ripple-effect bg-light-blue btn-social" href="{{url('auth/twitter')}}"><i class="fa fa-twitter"></i> | <span>Masuk lewat Twitter</span></a>
             <br/><br/>
             <h3>Kamu geek banget?</h3>
-            <a class="mdl-button mdl-button--colored mdl-button--raised mdl-js-button mdl-js-ripple-effect bg-black btn-social" href="{{url('auth/github')}}"><i class="fa fa-github"></i> <span>Masuk lewat Github</span></a>
+            <a class="mdl-button mdl-button--colored mdl-button--raised mdl-js-button mdl-js-ripple-effect bg-black btn-social" href="{{url('auth/github')}}"><i class="fa fa-github"></i> | <span>Masuk lewat Github</span></a>
         </div>
     </div>
 @endsection
